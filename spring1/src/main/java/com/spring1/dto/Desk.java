a1d442ffb7be1166bc4cf26c5cf0633736e9f03f
Kimmingle <t5328@naver.com>
2024-05-07 19:18:30 +0900

    first commit

---
+++ spring1/src/main/java/com/spring1/dto/Desk.java
@@ -0,0 +1,31 @@
+package com.spring1.dto;
+
+public class Desk implements Goods {  //굿즈를 당기면 데스크는 따라올것
+	
+	private double size;
+
+	@Override
+	public String toString() {
+		return "Desk [size=" + size + "]";
+	}
+
+	public Desk(double size) {
+		super();
+		this.size = size;
+	}
+
+	public double getSize() {
+		return size;
+	}
+
+	public void setSize(double size) {
+		this.size = size;
+	}
+
+	public Desk() {
+		super();
+	}
+	
+	
+
+}
---
+++ spring1/src/main/java/com/spring1/test/InjectionTest3.java
@@ -0,0 +1,23 @@
+package com.spring1.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import com.spring1.dto.Market;
+
+
+public class InjectionTest3 {
+// bean으로 만들어뒀기 때문에 getBean으로 불러올 수 있음
+
+	public static void main(String[] args) {
+		
+		ApplicationContext ctx1 = new GenericXmlApplicationContext("classpath:injectionContext3.xml");
+		
+		Market market1 = ctx1.getBean("Market1", Market.class);
+		System.out.println(market1);
+		
+		
+		
+	}
+
+}
